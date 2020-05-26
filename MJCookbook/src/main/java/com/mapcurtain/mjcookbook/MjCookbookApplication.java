e64a667406efa13f81fdc5164906c0b6d42f82dc
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-26 16:43:23 -0600

    add bcrypt bean, profile recipe user repo interfaces

--- MJCookbook/src/main/java/com/mapcurtain/mjcookbook/MjCookbookApplication.java
+++ MJCookbook/src/main/java/com/mapcurtain/mjcookbook/MjCookbookApplication.java
@@ -2,12 +2,20 @@ package com.mapcurtain.mjcookbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MjCookbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MjCookbookApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }

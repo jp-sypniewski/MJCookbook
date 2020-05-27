aeb85f1e3763cf4500ec54b5b231ace0ba6f1ed5
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-27 15:03:56 -0600

    MC45

--- ngMJCookbook/src/app/models/profile.ts
+++ ngMJCookbook/src/app/models/profile.ts
@@ -1,2 +1,13 @@
 export class Profile {
+  id: number;
+  firstName: string;
+  lastName: string;
+  description: string;
+
+  constructor(id?: number, firstName?: string, lastName?: string, description?: string){
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.description = description;
+  }
 }
--- ngMJCookbook/src/app/models/recipe.ts
+++ ngMJCookbook/src/app/models/recipe.ts
@@ -1,5 +1,14 @@
+import { User } from './user';
 export class Recipe {
   id: number;
-  title: String;
-  recipeText: String;
+  title: string;
+  recipeText: string;
+  user: User;
+
+  constructor(id?: number, title?: string, recipeText?: string, user?: User){
+    this.id = id;
+    this.title = title;
+    this.recipeText = recipeText;
+    this.user = user;
+  }
 }

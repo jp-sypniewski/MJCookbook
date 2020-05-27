99ef0ba51d4ea19746c8b338936f92da0e67c508
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-27 16:28:40 -0600

    stub methods in authsvc, inject auth into recipesvc

--- ngMJCookbook/src/app/services/auth.service.ts
+++ ngMJCookbook/src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,49 @@ export class AuthService {
   private baseUrl = 'http://localhost:8085/';
 
   constructor(private http: HttpClient) { }
+
+  login(username, password) {
+    const credentials = this.generateBasicAuthCredentials(username, password);
+    const httpOptions = {
+      headers: new HttpHeaders({
+         'Authorization': `Basic ${credentials}`,
+         'X-Requested-With': 'XMLHttpRequest'
+       })
+    };
+   return this.http
+   .get(this.baseUrl + 'authenticate', httpOptions)
+   .pipe(
+     tap((res) => {
+       localStorage.setItem('credentials' , credentials);
+       return res;
+     }),
+     catchError((err: any) => {
+       console.log(err);
+       return throwError('UserService.login(): Error logging in.');
+     })
+   );
+  }
+
+  // removes local creds
+  logout() {
+    localStorage.removeItem('credentials');
+  }
+
+  // checks for local creds
+  checkLogin() {
+    if (localStorage.getItem('credentials')) {
+      return true;
+    }
+    return false;
+  }
+
+  // generates creds (Basic)
+  generateBasicAuthCredentials(username, password) {
+    return btoa(`${username}:${password}`);
+  }
+
+  // returns creds to be used in other api calls
+  getCredentials() {
+    return localStorage.getItem('credentials');
+  }
 }
--- ngMJCookbook/src/app/services/recipe.service.ts
+++ ngMJCookbook/src/app/services/recipe.service.ts
@@ -1,6 +1,9 @@
+import { AuthService } from './auth.service';
 import { Recipe } from 'src/app/models/recipe';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +12,28 @@ export class RecipeService {
 
   private baseUrl = 'http://localhost:8085/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private authSvc: AuthService) { }
 
   getAllRecipes(){
-    const credentials = "";//this.userSvc.getCredentials();
+    const credentials = this.authSvc.getCredentials();
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': `Basic ${credentials}`
       })
     };
+    return this.http.get<Recipe[]>(this.baseUrl + 'api/recipes', httpOptions)
+    .pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('RecipeService.getAllRecipes(): error getting recipes.');
+      })
+    );
   }
 
   createRecipe(recipe: Recipe){
-    const credentials = "";//this.userSvc.getCredentials();
+    const credentials = this.authSvc.getCredentials();
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
@@ -32,7 +43,7 @@ export class RecipeService {
   }
 
   updateRecipe(recipe: Recipe){
-    const credentials = "";//this.userSvc.getCredentials();
+    const credentials = this.authSvc.getCredentials();
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',

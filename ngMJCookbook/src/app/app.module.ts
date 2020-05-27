c6755f92ed48a95035c3a149f393071756511734
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-27 15:46:31 -0600

    MC55-58

--- ngMJCookbook/src/app/app.module.ts
+++ ngMJCookbook/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,8 @@ import { RecipeListComponent } from './components/recipe-list/recipe-list.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ngMJCookbook/src/app/components/recipe-list/recipe-list.component.ts
+++ ngMJCookbook/src/app/components/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { User } from 'src/app/models/user';
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe';
 
@@ -8,12 +9,43 @@ import { Recipe } from 'src/app/models/recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-  title: String = "Recipe List";
+  title: string = "Recipe List";
   recipes: Recipe[] = [];
+  singleView: boolean = false;
+  selectedRecipe: Recipe = new Recipe();
+  showForm: boolean = false;
+
+
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectARecipe(recipe: Recipe){
+    this.selectedRecipe = recipe;
+    this.singleView = true;
+  }
+
+  editRecipe(){
+    this.showForm = true;
+  }
+
+  createNewRecipe(){
+    this.selectedRecipe = new Recipe();
+    this.showForm = true;
+  }
+
+  submit(){
+    if (this.selectedRecipe.id != null){
+      // if it has an id, then submit the put
+    } else {
+      // otherwise submit the post
+    }
+    // reassign it to the selected one to show the new one
+    this.showForm = false;
   }
 
 }

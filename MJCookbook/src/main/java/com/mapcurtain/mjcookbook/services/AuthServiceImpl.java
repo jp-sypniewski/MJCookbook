5c93bb2ac46b43e56e309583a4bb6887c6d95fa3
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-26 20:55:08 -0600

    add service annotations, fill application.properties, add classes to persistence, successful launch of spring boot project

--- MJCookbook/src/main/java/com/mapcurtain/mjcookbook/services/AuthServiceImpl.java
+++ MJCookbook/src/main/java/com/mapcurtain/mjcookbook/services/AuthServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.mapcurtain.mjcookbook.entities.User;
 import com.mapcurtain.mjcookbook.repositories.ProfileRepository;
 import com.mapcurtain.mjcookbook.repositories.UserRepository;
 
+@Service
 public class AuthServiceImpl implements AuthService {
 	
 	@Autowired
--- MJCookbook/src/main/java/com/mapcurtain/mjcookbook/services/RecipeServiceImpl.java
+++ MJCookbook/src/main/java/com/mapcurtain/mjcookbook/services/RecipeServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mapcurtain.mjcookbook.entities.Recipe;
 import com.mapcurtain.mjcookbook.repositories.RecipeRepository;
 
+@Service
 public class RecipeServiceImpl implements RecipeService {
 	
 	@Autowired

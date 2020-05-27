d6e5dadbd3276b26548f9e287a27a2517b1ffa96
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-26 20:43:05 -0600

    add endpoints for register authenticate and update user

--- MJCookbook/src/main/java/com/mapcurtain/mjcookbook/controllers/AuthController.java
+++ MJCookbook/src/main/java/com/mapcurtain/mjcookbook/controllers/AuthController.java
@@ -1,5 +1,65 @@
 package com.mapcurtain.mjcookbook.controllers;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mapcurtain.mjcookbook.entities.User;
+import com.mapcurtain.mjcookbook.services.AuthService;
+
+@RestController
+@CrossOrigin({ "*", "http://localhost:4200" })
 public class AuthController {
+	
+	@Autowired
+	private AuthService authSvc;
+	
+	@GetMapping(path="/authenticate")
+	public Principal authenticate(Principal principal) {
+		return principal;
+	}
+	
+	@PostMapping(path="/register")
+	public User register(HttpServletResponse res,
+			@RequestBody User user) {
+		user = authSvc.register(user);
+		if (user != null) {
+			res.setStatus(201);
+			return user;
+		} else {
+			res.setStatus(400);
+			return null;
+		}
+	}
+	
+	@PutMapping(path="/update")
+	public User update(HttpServletResponse res,
+			Principal principal,
+			@RequestBody User user) {
+		try {
+			user = authSvc.update(user, principal.getName());
+			if (user != null) {
+				res.setStatus(202);
+				return user;
+			} else {
+				res.setStatus(404);
+				return null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+			return null;
+		}
+		
+		
+	}
 
 }
--- MJCookbook/src/main/java/com/mapcurtain/mjcookbook/services/AuthServiceImpl.java
+++ MJCookbook/src/main/java/com/mapcurtain/mjcookbook/services/AuthServiceImpl.java
@@ -1,5 +1,47 @@
 package com.mapcurtain.mjcookbook.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.mapcurtain.mjcookbook.entities.User;
+import com.mapcurtain.mjcookbook.repositories.ProfileRepository;
+import com.mapcurtain.mjcookbook.repositories.UserRepository;
+
 public class AuthServiceImpl implements AuthService {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private ProfileRepository profileRepo;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Override
+	public User register(User user) {
+		String encodedPw = encoder.encode(user.getPassword());
+		user.setPassword(encodedPw);
+		user.setProfile(profileRepo.saveAndFlush(user.getProfile()));
+		user = userRepo.saveAndFlush(user);
+		return user;
+	}
+	
+	@Override
+	public User update(User user, String username) {
+		Optional<User> opt = userRepo.findById(username);
+		if (opt.isPresent()) {
+			User managedUser = opt.get();
+			managedUser.setProfile(profileRepo.saveAndFlush(user.getProfile()));
+			managedUser.setPassword(encoder.encode(user.getPassword()));
+			managedUser = userRepo.saveAndFlush(managedUser);
+			return managedUser;
+		} else {
+			return null;
+		}
+	}
+	
 
 }

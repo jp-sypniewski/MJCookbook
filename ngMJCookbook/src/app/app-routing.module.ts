c99bd35a4229214acef160bad5df89389266908f
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-28 15:10:05 -0600

    MC98

--- ngMJCookbook/src/app/app-routing.module.ts
+++ ngMJCookbook/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { AccountComponent } from './components/account/account.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'recipeList', component: RecipeListComponent},
+  {path: 'account', component: AccountComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

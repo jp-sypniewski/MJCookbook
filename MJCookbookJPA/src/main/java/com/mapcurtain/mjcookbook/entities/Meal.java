cf4dbbc8db0ddc992c04b69abca44612bda4a774
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-28 23:04:21 -0600

    MC84

--- MJCookbookJPA/src/main/java/com/mapcurtain/mjcookbook/entities/Meal.java
+++ MJCookbookJPA/src/main/java/com/mapcurtain/mjcookbook/entities/Meal.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -34,5 +36,127 @@ public class Meal {
 	@Column(name="updated_at")
 	@UpdateTimestamp
 	private LocalDateTime updatedAt;
+	
+	@ManyToOne
+	@JoinColumn(name="recipe_id")
+	private Recipe recipe;
+	
+	@ManyToOne
+	@JoinColumn(name="user_username")
+	private User user;
+
+	public Meal() {
+		super();
+	}
+
+	public Meal(int id, Integer rating, Boolean completed, Boolean enabled, LocalDateTime plannedFor,
+			LocalDateTime createdAt, LocalDateTime updatedAt, Recipe recipe, User user) {
+		super();
+		this.id = id;
+		this.rating = rating;
+		this.completed = completed;
+		this.enabled = enabled;
+		this.plannedFor = plannedFor;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.recipe = recipe;
+		this.user = user;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+	public Boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public LocalDateTime getPlannedFor() {
+		return plannedFor;
+	}
+
+	public void setPlannedFor(LocalDateTime plannedFor) {
+		this.plannedFor = plannedFor;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Meal other = (Meal) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
 
 }

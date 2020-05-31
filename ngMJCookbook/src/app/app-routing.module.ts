be003efe066de66fbd836081645384d3bb067ca8
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-31 17:01:32 -0600

    MC107

--- ngMJCookbook/src/app/app-routing.module.ts
+++ ngMJCookbook/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './components/home/home.component';
 import { MealComponent } from './components/meal/meal.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,6 +7,8 @@ import { AccountComponent } from './components/account/account.component';
 
 
 const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'recipeList', component: RecipeListComponent},
   {path: 'account', component: AccountComponent},
   {path: 'meals', component: MealComponent},

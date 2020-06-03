fe3c9b513915a9488fa6bd5805f1c2ff3d5dd195
jp-sypniewski <jpsypniewski@icloud.com>
2020-06-03 14:16:23 -0600

    MC178

--- ngMJCookbook/src/app/services/recipe.service.ts
+++ ngMJCookbook/src/app/services/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Instruction } from './../models/instruction';
 import { AuthService } from './auth.service';
 import { Recipe } from 'src/app/models/recipe';
 import { Injectable } from '@angular/core';
@@ -63,6 +64,25 @@ export class RecipeService {
         console.log(err);
         return throwError('RecipeService.updateRecipe(): error updating recipe.');
       })
+    );
+  }
+
+  updateInstructionsAndIngredients(recipe: Recipe){
+    const credentials = this.authSvc.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${credentials}`
+      })
+    };
+    return this.http.post<Instruction[]>(this.baseUrl + 'api/recipes/' +
+      recipe.id + '/instructions', recipe.instructions, httpOptions)
+      .pipe(
+        catchError((err:any) => {
+          console.log(err);
+          return throwError('RecipeService.updateInstructionsAndIngredients(): error with inst update');
+        }
+      )
     );
   }
 

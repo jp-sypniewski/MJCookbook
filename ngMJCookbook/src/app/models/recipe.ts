eaf486cbc6d34e0fbee20ec2a960403c19580240
jp-sypniewski <jpsypniewski@icloud.com>
2020-06-03 12:08:50 -0600

    MC168

--- ngMJCookbook/src/app/models/recipe.ts
+++ ngMJCookbook/src/app/models/recipe.ts
@@ -1,14 +1,26 @@
+import { Ingredient } from './ingredient';
+import { Instruction } from './instruction';
 import { User } from './user';
 export class Recipe {
   id: number;
   title: string;
   recipeText: string;
   user: User;
+  enabled: Boolean;
+  status: String;
+  instructions: Instruction[];
+  ingredients: Ingredient[];
 
-  constructor(id?: number, title?: string, recipeText?: string, user?: User){
+  constructor(id?: number, title?: string, recipeText?: string, user?: User,
+    enabled?: Boolean, status?: String,
+    instructions?: Instruction[], ingredients?: Ingredient[]){
     this.id = id;
     this.title = title;
     this.recipeText = recipeText;
     this.user = user;
+    this.enabled = enabled;
+    this.status = status;
+    this.instructions = instructions;
+    this.ingredients = ingredients;
   }
 }

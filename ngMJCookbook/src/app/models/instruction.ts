ebc4e0655620462f9a2112b12c3f0be91aa13950
jp-sypniewski <jpsypniewski@icloud.com>
2020-06-03 15:57:16 -0600

    MC187

--- ngMJCookbook/src/app/models/instruction.ts
+++ ngMJCookbook/src/app/models/instruction.ts
@@ -2,15 +2,15 @@ import { Recipe } from 'src/app/models/recipe';
 import { Ingredient } from './ingredient';
 export class Instruction {
   id: Number;
-  order: Number;
+  sequence: Number;
   text: String;
   ingredients: Ingredient[];
   recipe: Recipe;
 
-  constructor(id?: Number, order?: Number, text?: String,
+  constructor(id?: Number, sequence?: Number, text?: String,
     ingredients?: Ingredient[], recipe?: Recipe){
     this.id = id;
-    this.order = order;
+    this.sequence = sequence;
     this.text = text;
     this.ingredients = ingredients;
     this.recipe = recipe;
